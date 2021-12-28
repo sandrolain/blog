494788d267266921b8aae1c15289bd71b0afa3c0
Sandro Lain <xande05@gmail.com>
2021-12-28 16:35:09 +0100

    Fix Signature image src

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -1,5 +1,5 @@
 import signatureImg from "./3ad39970a957a5f28905d8030813c3fb.png"
 
-export const Signature = () => <div style={{textAlign: "left"}}><img src={signatureImg} style={{
+export const Signature = () => <div style={{textAlign: "left"}}><img src={signatureImg.src} style={{
   height: "32px"
 }} /></div>;
