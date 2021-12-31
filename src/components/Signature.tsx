d068a252a848d4d10d07c4d1d1b82ef54e78421f
Sandro Lain <xande05@gmail.com>
2021-12-31 14:55:06 +0100

    First post signature fix

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -1,5 +1,5 @@
 import signatureImg from "./3ad39970a957a5f28905d8030813c3fb.png"
 
-export const Signature = () => <div style={{textAlign: "left"}}><img src={signatureImg.src} style={{
-  height: "32px"
-}} /></div>;
+export default function Signature () {
+  return <div style={{textAlign: "left"}}><img src={signatureImg.src} style={{height: "32px"}} /></div>
+}
